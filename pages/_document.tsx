429e685c5592d8d004dca45f55bdc47a39bab10c
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2025-03-27 15:14:51 +0000

    add ga support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
+      <Head>
+      </Head>
+        <body className="antialiased">
+        <Main/>
+        <NextScript/>
+        <script data-nscript={"lazyOnload"}
+                src="https://www.googletagmanager.com/gtag/js?id=G-ZP0CQ3RP8K"></script>
+        <script id={'ga-script'} data-nscript={"lazyOnload"}>
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-ZP0CQ3RP8K');
+                `}
+        </script>
+        </body>
     </Html>
   );
 }
